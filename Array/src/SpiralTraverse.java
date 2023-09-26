c4f610fd0aa49e47289ff60260b031ea5cfa8982
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-09-26 13:00:53 +0100

    SpiralTraverse

---
+++ Array/src/SpiralTraverse.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SpiralTraverse {
+    public static void main(String[] args) {
+        int[][] input = new int[][] {
+                {1, 2, 3, 4},
+                {12, 13, 14, 5},
+                {11, 16, 15, 6},
+                {10, 9, 8, 7},
+        };
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+        List<Integer> actual = spiralTraverse(input);
+        System.out.println(expected);
+        System.out.println(actual);
+    }
+
+    public static List<Integer> spiralTraverse(int[][] array) {
+        if(array.length == 0) return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        int startRow = 0;
+        int endRow = array.length - 1;
+        int startCol = 0;
+        int endCol = array[0].length - 1;
+
+        while(startRow <= endRow && startCol <= endCol) {
+            // top
+            for(int col = startCol; col <= endCol; col++) {
+                result.add(array[startRow][col]);
+            }
+            // right
+            for(int row = startRow + 1; row <= endRow; row++) {
+                result.add(array[row][endCol]);
+            }
+            // bottom
+            for(int col = endCol - 1; col >= startCol; col--) {
+                if(startRow == endRow) break;
+                result.add(array[endRow][col]);
+            }
+            // left
+            for(int row = endRow - 1; row > startRow; row--) {
+                if(startCol == endCol) break;
+                result.add(array[row][startCol]);
+            }
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+        return result;
+    }
+}
