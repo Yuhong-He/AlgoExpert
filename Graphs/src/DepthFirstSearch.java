01cba50c481af7e1d03f89a267ae5a1f0b1b5916
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-10-24 13:50:36 +0100

    DepthFirstSearch

---
+++ Graphs/src/DepthFirstSearch.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DepthFirstSearch {
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            array.add(this.name);
+            for (Node child : children) {
+                child.depthFirstSearch(array);
+            }
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node graph = new Node("A");
+        graph.addChild("B").addChild("C").addChild("D");
+        graph.children.get(0).addChild("E").addChild("F");
+        graph.children.get(2).addChild("G").addChild("H");
+        graph.children.get(0).children.get(1).addChild("I").addChild("J");
+        graph.children.get(2).children.get(0).addChild("K");
+        String[] expected = {"A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"};
+        List<String> inputArray = new ArrayList<>();
+        List<String> actual = graph.depthFirstSearch(inputArray);
+        System.out.println("expected: \t" + Arrays.toString(expected));
+        System.out.println("actual: \t" + Arrays.toString(actual.toArray()));
+    }
+}
